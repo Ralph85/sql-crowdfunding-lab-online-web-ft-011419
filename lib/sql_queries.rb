0d192513fe25fb2cb5585a461bc68fbc822cce9b
Ralph Saint Albord <ralph.saintalbord@yahoo.com>
2019-03-04 03:58:31 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -16,7 +16,7 @@ end
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
 "SELECT name, age, SUM(pledges.amount) FROM users
 JOIN pledges ON pledges.user_id = user.id
-GROUP BY pledge_amount
+GROUP BY user_id
 ORDER BY name"
 end
 

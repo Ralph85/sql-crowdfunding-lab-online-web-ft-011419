aa71adac2dacb96dd27b4e7201535ca4ff51bca1
Ralph Saint Albord <ralph.saintalbord@yahoo.com>
2019-03-04 04:11:40 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -21,7 +21,7 @@ ORDER BY name"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT title, SUM(pledges.amount) - funding_goal FROM projects
+"SELECT title, SUM(pledges.amount) FROM projects
 JOIN pledges ON pledges.project_id = projects.id
 WHERE pledges.amount > funding_goal
 GROUP BY project_id"

03f365076e8f040d6ea9ccfdaf8ce1109f852ec5
Ralph Saint Albord <ralph.saintalbord@yahoo.com>
2019-03-04 04:24:25 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -35,7 +35,9 @@ ORDER BY SUM(pledges.amount), name"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"Write your SQL query Here"
+"SELECT name, SUM(pledges.amount) FROM projects
+JOIN pledges ON pledges.project_id = projects.id
+GROUP BY project"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
